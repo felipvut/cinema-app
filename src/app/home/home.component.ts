acf74093a4f8f104d309747dd7e87523de2c0e3d
Felipe Ferreira <felipvut@gmail.com>
2024-07-31 15:07:06 -0300

    Adicionando a primeira tela do sistema

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+})
+export class HomeComponent {
+
+}
