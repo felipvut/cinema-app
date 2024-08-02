76dd3b4a7515165cebb18549688b895f8e02734e
Felipe Ferreira <felipvut@gmail.com>
2024-08-02 20:03:04 -0300

    Adicionando informações sobre o filme em detalhes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'details/:uuid', component: DetailsComponent},
     {path: '**', redirectTo: 'home'}
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,36 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+type Movie = {
+  uuid: string,
+  thumbnail: string
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ RouterLink, CommonModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit{
+
+  movies: Movie[] = [
+    { uuid: '1', thumbnail: 'https://ingresso-a.akamaihd.net/prd/img/movie/meu-malvado-favorito-4/4d756249-3b88-4b59-baa2-527f5e3d4c3e.webp'},
+    { uuid: '2', thumbnail: 'https://ingresso-a.akamaihd.net/prd/img/movie/deadpool-e-wolverine/09f1e988-47fe-4d5e-88ec-a51051d3241f.webp'},
+    { uuid: '3', thumbnail: 'https://ingresso-a.akamaihd.net/prd/img/movie/divertida-mente-2/61ac248d-e3e6-4e33-9515-8ce0621a32fa.webp'},
+    { uuid: '1', thumbnail: 'https://ingresso-a.akamaihd.net/prd/img/movie/meu-malvado-favorito-4/4d756249-3b88-4b59-baa2-527f5e3d4c3e.webp'},
+    { uuid: '2', thumbnail: 'https://ingresso-a.akamaihd.net/prd/img/movie/deadpool-e-wolverine/09f1e988-47fe-4d5e-88ec-a51051d3241f.webp'},
+    { uuid: '3', thumbnail: 'https://ingresso-a.akamaihd.net/prd/img/movie/divertida-mente-2/61ac248d-e3e6-4e33-9515-8ce0621a32fa.webp'},
+  ]
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      const swipperContainer: any = document.getElementById('swiper-container')
+      swipperContainer.style.width = "100%"
+    }, 100)
+  }
 }
